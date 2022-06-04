0f767cb7e257757473799cd0e5eaa8d4a8773c7d
Raman kumar <32980600+Raman-Kumar@users.noreply.github.com>
2022-06-04 18:19:19 +0530

    init project

---
+++ app/src/main/java/com/raman/createanduselib/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.raman.createanduselib
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.raman.mylibrary.ScreenChanges
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
---
+++ mylibrary/src/main/java/com/raman/mylibrary/ScreenChanges.kt
@@ -0,0 +1,11 @@
+package com.raman.mylibrary
+
+import android.content.Context
+import android.content.Context.MODE_PRIVATE
+
+import android.content.SharedPreferences
+
+
+class ScreenChanges {
+
+}
